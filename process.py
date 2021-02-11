c3e6fc33138d03fe96e6a19343d7e68be9a986b4
Áine Thompson <ainegthompson@gmail.com>
2021-02-11 19:29:57 +0000

    Completed process.py

--- process.py
+++ process.py
@@ -1,12 +1,14 @@
-log_file = open("um-server-01.txt")
 
-def sales_reports(log_file):   
+log_file = open("um-server-01.txt")
+# open file with data
+
+def sales_reports(log_file):   #define function that takes in file
 
     for line in log_file:
-        line = line.rstrip()  
-        day = line[0:3]     
-        if day == "Tues":    
+        line = line.rstrip()   #for each line, remove extra whitespace
+        day = line[0:3]     #day = first 3 chars of line
+        if day == "Mon":    #if first 3 chars of line are "Mon", print line
             print(line)
 
 
-sales_reports(log_file)
+sales_reports(log_file)       #call function 
